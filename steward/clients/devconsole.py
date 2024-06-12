155288ac8ffefa4a1047e474f13fa5331d2340e4
Shawn <shawn@shawngrover.ca>
2024-06-12 05:34:43 -0600

    renamed to devconsole.set the client_type.

--- steward/clients/devconsole.py
+++ steward/clients/devconsole.py
@@ -2,7 +2,7 @@ from steward.clients.base_client import BaseClient
 from steward.logger import COLORS
 from steward.event import StewardEvent
 
-class ConsoleClient(BaseClient):
+class DevConsoleClient(BaseClient):
     def __init__(self, *args, **kwargs):
         # Extract specific parameters if needed
         # self.other_param = kwargs.pop('other_param', None)
@@ -11,11 +11,10 @@ class ConsoleClient(BaseClient):
 
         self.intro = "I am your steward. How can I help?\n"
         self.prompt = f"{COLORS['GREEN']}Steward: {COLORS['DEFAULT']}"
+        self.client_type = 'DevConsole'
 
     def _handle_message(self, message):
-        self.logger.info(f'[CONSOLE] MSG: {message}')
-        if message == 'welcome':
-            self.send("thanks")
+        self.logger.debug(f'[CONSOLE] MSG: {message}')
         
         if isinstance(message, StewardEvent):
             if message.name == "STEWARD_STOPPING":
